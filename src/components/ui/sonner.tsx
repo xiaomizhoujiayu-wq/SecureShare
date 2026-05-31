9735e2b6f8f7e61a7a2854d15d2f242e99a63d28
acdd <89513811+acdd233@users.noreply.github.com>
2026-05-31 20:58:36 +1000

    chore: apply GPL-3.0-or-later license and add copyright headers
    This commit includes:
    - Updating package.json to reflect the GPL-3.0-or-later license and removing the private flag
    - Adding the GNU General Public License v3.0 text to the root LICENSE file
    - Prepending the GPLv3 copyright header to all source files in the src/ directory with contributors Yumi/acdd233/puchen-star

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2026 Yumi/acdd233/puchen-star
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 import { useTheme } from "next-themes"
 import { Toaster as Sonner, type ToasterProps } from "sonner"
 import { CircleCheckIcon, InfoIcon, TriangleAlertIcon, OctagonXIcon, Loader2Icon } from "lucide-react"
